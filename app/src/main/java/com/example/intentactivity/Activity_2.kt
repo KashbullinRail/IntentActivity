39fd98505e9953b1345357679f55ea857a7543b1
KashbullinRail <kashbullinrail@gmail.com>
2022-10-29 16:16:32 +0300

    организавана передача данных между активити

--- app/src/main/java/com/example/intentactivity/Activity_2.kt
+++ app/src/main/java/com/example/intentactivity/Activity_2.kt
@@ -2,6 +2,7 @@ package com.example.intentactivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intentactivity.databinding.Activity2Binding
@@ -14,11 +15,22 @@ class Activity_2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = Activity2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val message = intent.getStringExtra(KEY)
+        binding.tvMessage.text = message
+
     }
 
-    fun onClickGoAct_3(view: View) {
-        val intent = Intent(this, Activity_3::class.java)
-        startActivity(intent)
+    fun onClickGoAct_1(view: View) {
+        intent.putExtra(KEY2, binding.etAnswer.text.toString())
+        setResult(RESULT_OK, intent)
+        Log.d("TAGGER", "setResult")
+        finish()
+    }
+
+    fun onClickGoAct_3(view: View){
+        val i = Intent(this, Activity_3::class.java)
+        startActivity(i)
     }
 
 }
---
+++ app/src/main/java/com/example/intentactivity/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.intentactivity
+
+const val KEY = "key"
+const val KEY2 = "key2"
+const val REQUEST_CODE = 50
